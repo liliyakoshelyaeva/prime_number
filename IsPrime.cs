a18b2ec4e431bfa4e1a9dadb71612325ed43b848
maarr1 <146519251+maarr1@users.noreply.github.com>
2025-01-07 01:06:16 +0300

    Add files via upload
    ,

---
+++ IsPrime.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+
+namespace prime_number
+{
+    internal class prime_number
+    {
+        public static bool IsPrime(int number)
+        {
+            for (int i = 2; i < Math.Sqrt(number); i++)
+            {
+                if (number % i == 0)
+                { return false; }
+            }
+
+            return true;
+        }
+        public static void Main()
+        {
+            Console.Write("Введите целое число: ");
+
+            if (int.TryParse(Console.ReadLine(), out int number))
+            {
+                if (IsPrime(number))
+                {
+                    Console.WriteLine($"{number} - простое число.");
+                }
+                else
+                {
+                    Console.WriteLine($"{number} - не является простым числом.");
+                }
+            }
+            else
+            {
+                Console.WriteLine("Некорректный ввод.");
+            }
+            Console.ReadKey();
+        }
+    }
+
+}
